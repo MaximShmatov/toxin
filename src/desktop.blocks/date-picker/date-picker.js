74c691fa1a6ea4783bc861cde58da19feb3b528f
MaximShmatov <externgroupmail@mail.ru>
2020-05-28 06:21:09 +0300

    Create block date-range

--- src/desktop.blocks/date-picker/date-picker.js
+++ src/desktop.blocks/date-picker/date-picker.js
@@ -9,8 +9,7 @@ export class DatePicker {
   week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
   month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябр', 'Октябрь', 'Ноябрь', 'Декабрь'];
   #days = [];
-  #currentDay;
-
+  #day;
   #datePicker = $('.date-picker');
   #listLeft = this.#datePicker.find('.date-picker__list-left');
   #listRight = this.#datePicker.find('.date-picker__list-right');
@@ -23,7 +22,7 @@ export class DatePicker {
   constructor(date) {
     if (date) {
       this.#date = date;
-      this.#currentDate.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
+      this.#currentDate.setTime(date.getTime());
     } else this.#date = new Date();
 
     for (let i = 0; i < 35; i++) {
@@ -45,20 +44,27 @@ export class DatePicker {
 
     this.#setTitle();
     this.#setDatePicker();
-    this.#currentDay = this.#datePicker.find(`button:contains(${this.#date.getDate()})`);
-    this.#focusCurrentDate();
+    document.addEventListener('mouseup', this.#hiddenPicker.bind(this));
   }
 
   togglePicker() {
     this.#datePicker.toggleClass('date-picker_visibility');
   }
-  get selectedDate () {
+
+  get selectedDate() {
     return this.#currentDate;
   }
 
-  #focusCurrentDate() {
-    if (this.#date.getMonth() === this.#currentDate.getMonth()) this.#currentDay.addClass('date-picker__table-date_current');
-    else this.#currentDay.removeClass('date-picker__table-date_current');
+  get date() {
+    return this.#date;
+  }
+
+  get days() {
+    return this.#days;
+  }
+
+  #hiddenPicker(evt) {
+    if (this.#datePicker.has(evt.target).length === 0) this.#datePicker.addClass('date-picker_visibility');
   }
 
   #setTitle() {
@@ -69,28 +75,27 @@ export class DatePicker {
     this.#currentDate.setMonth(this.#currentDate.getMonth() - 1);
     this.#setTitle(this.#currentDate);
     this.#setDatePicker();
-    this.#focusCurrentDate();
   }
 
   #listRightDate() {
     this.#currentDate.setMonth(this.#currentDate.getMonth() + 1);
     this.#setTitle(this.#currentDate);
     this.#setDatePicker();
-    this.#focusCurrentDate();
+    this.#datePicker.trigger('date_list');
   }
 
   #clearPicker() {
-    this.#currentDate.setFullYear(this.#date.getFullYear(), this.#date.getMonth(), this.#date.getDate());
+    this.#currentDate.setTime(this.#date.getTime());
     this.#setTitle();
     this.#setDatePicker();
-    this.#focusCurrentDate();
+    this.#datePicker.trigger('date_clear');
+
   }
 
   #selectedDate(evt) {
-    this.#currentDate.setDate(Number($(evt.currentTarget).text()));
-    this.#currentDate.setMonth(Number($(evt.currentTarget).attr('data-month')));
-    this.#currentDate.setFullYear(Number($(evt.currentTarget).attr('data-year')));
+    this.#currentDate.setTime($(evt.currentTarget).attr('data-timestamp'));
     $(evt.currentTarget).addClass('date-picker__table-date_selected');
+    this.#datePicker.trigger('date_selected');
   }
 
   #unselectedDate(evt) {
@@ -107,14 +112,18 @@ export class DatePicker {
     this.#currentDate.setDate(-weekDay + 1);
     for (let i = 0; i < 35; i++) {
       this.#currentDate.setDate(this.#currentDate.getDate() + 1);
-      if (this.#currentDate.getMonth() === currentMonth) this.#days[i].addClass('date-picker__table-date_month-current');
-      else this.#days[i].removeClass('date-picker__table-date_month-current');
-      this.#days[i].attr('data-year', this.#currentDate.getFullYear());
-      this.#days[i].attr('data-month', this.#currentDate.getMonth());
+      if (this.#currentDate.getMonth() === currentMonth) {
+        this.#days[i].addClass('date-picker__table-date_month-current');
+        let isCurrentDate = this.#currentDate.getDate() === this.#date.getDate() && currentMonth === this.#date.getMonth() && currentYear === this.#date.getFullYear();
+        if (isCurrentDate) {
+          this.#days[i].addClass('date-picker__table-date_current');
+          this.#day = this.#days[i];
+        }
+      } else this.#days[i].removeClass('date-picker__table-date_month-current');
+      this.#days[i].attr('data-timestamp', this.#currentDate.getTime());
       this.#days[i].text(this.#currentDate.getDate());
     }
+    if (currentMonth !== this.#date.getMonth()) this.#day.removeClass('date-picker__table-date_current');
     this.#currentDate.setFullYear(currentYear, currentMonth, currentDay);
   }
 }
-
-$.fn.datePicker = new DatePicker();
--- src/desktop.blocks/date-range/date-range.js
+++ src/desktop.blocks/date-range/date-range.js
@@ -1,36 +1,84 @@
 'use strict'
 
+import {DatePicker} from "../date-picker/date-picker";
+
 import('./date-range.sass');
 import('../button/button');
-import('../date-picker/date-picker');
 
-;(function ($) {
-  $.fn.dateRange = function () {
+class DateRange extends DatePicker {
+  #dateRange = $('.date-range');
+  #input = this.#dateRange.find('.date-range__date-dropdown-input');
+  #output = this.#dateRange.find('.date-range__date-dropdown-output');
+  #dropdownIn = this.#dateRange.find('.date-range__date-dropdown-in');
+  #dropdownOut = this.#dateRange.find('.date-range__date-dropdown-out');
+  #defaultDate = 'дд.мм.гггг';
+  dateIn = new Date();
+  dateOut = new Date();
+  #currentPicker;
 
-    let $dateRange = $('.date-range');
-    let input = $dateRange.find('.date-range__entry-input');
-    let dropdown = $dateRange.find('.date-range__entry-dropdown');
-    let picker = $dateRange.find('.date-range__picker');
-    let pickerList = $dateRange.find('.date-range__picker-list');
-    let pickerTitle = $dateRange.find('.date-range__picker-title');
-    let pickerTableDate = $dateRange.find('.date-range__picker-table-date');
-    let pickerButton = $dateRange.find('.date-range__picker-button');
-    
-    function pickerHidden(evt) {
-      if (picker.has(evt.target).length === 0) picker.css('display', 'none');
-    }
-    function togglePicker() {
-      if (picker.css('display') === 'none') {
-        picker.css('display', 'block');
-      } else picker.css('display', 'none');
-    }
+  constructor(date) {
+    super(date);
+    this.togglePicker();
+    this.#input.val(this.#defaultDate);
+    this.#output.val(this.#defaultDate);
+    this.#dateRange.on('date_selected', this.#setDate.bind(this));
+    this.#dateRange.on('date_clear', this.#clearDate.bind(this));
+    this.#dateRange.on('date_list', this.#listDate.bind(this));
+    this.#dropdownIn.on('click', this.#setDateIn.bind(this));
+    this.#dropdownOut.on('click', this.#setDateOut.bind(this));
 
-    dropdown.on('click', togglePicker);
-
-    document.addEventListener('mouseup', pickerHidden);
-
-    return this;
   }
-})($);
+  #setDateIn() {
+    this.togglePicker();
+    this.#currentPicker = 'in';
+  }
+  #setDateOut() {
+    this.togglePicker();
+    this.#currentPicker = 'out';
+  }
+  #clearDate() {
+    this.#input.val(this.#defaultDate);
+    this.#output.val(this.#defaultDate);
+    this.dateIn.setTime(this.date.getTime());
+    this.dateOut.setTime(this.date.getTime());
+    this.#setDateRange(this.dateIn, this.dateOut);
+  }
+  #listDate() {
+    this.#setDateRange();
+  }
+  #setDate() {
+    let date = `${this.selectedDate.getDate()}.${this.selectedDate.getMonth()}.${this.selectedDate.getFullYear()}`;
 
-$('.date-range').dateRange();
+    if (this.#currentPicker === 'in') {
+      this.#input.val(date);
+      this.dateIn.setTime(this.selectedDate.getTime());
+    }
+
+    if (this.#currentPicker === 'out') {
+      this.#output.val(date);
+      this.dateOut.setTime(this.selectedDate.getTime());
+    }
+    this.#setDateRange(this.dateIn, this.dateOut);
+  }
+
+  #setDateRange() {
+    let tableDate = $('.date-picker__table td');
+    let date = new Date();
+    for (let i = 0; i < 35; i++) {
+      $(tableDate[i]).removeClass('date-range__date-picker_range');
+      $(tableDate[i]).removeClass('date-range__date-picker_range-first');
+      $(tableDate[i]).removeClass('date-range__date-picker_range-last');
+      date.setTime(Number(this.days[i].attr('data-timestamp')));
+      if (date >= this.dateIn && date <= this.dateOut) {
+        $(tableDate[i]).addClass('date-range__date-picker_range');
+      }
+    }
+    tableDate.filter('.date-range__date-picker_range:first').addClass('date-range__date-picker_range-first')
+      //.removeClass('date-range__date-picker_range');
+    tableDate.filter('.date-range__date-picker_range:last').addClass('date-range__date-picker_range-last')
+     // .removeClass('date-range__date-picker_range');
+
+  }
+}
+
+$.fn.dateRange = new DateRange(new Date());

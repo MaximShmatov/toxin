f9c4c8960f6e7cae4baad68c390696d6fb93ba8b
MaximShmatov <externgroupmail@mail.ru>
2020-06-21 14:46:43 +0300

    Create subscription block

--- src/desktop.blocks/page-uikit-form/page-uikit-form.js
+++ src/desktop.blocks/page-uikit-form/page-uikit-form.js
@@ -1,7 +1,8 @@
 'use strict'
 
-import('./page-uikit-form.sass');
-import('../dropdown-guests/dropdown-guests');
-import('../date-range/date-range');
-import('../date-filter/date-filter');
-import('../text-field/text-field');
+import './page-uikit-form.sass';
+import '../dropdown-guests/dropdown-guests';
+import '../date-range/date-range';
+import '../date-filter/date-filter';
+import '../text-field/text-field';
+import '../subscription/subscription';

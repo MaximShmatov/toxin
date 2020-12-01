f7413fcc6a6436383be778a8ff5cc88ca160f829
Maxim Shmatov <ExternGroupMail@mail.ru>
2020-12-01 10:00:48 +0300

    Refactoring block 'checkbox-rich' #5

---
+++ src/components/checkbox-rich/checkbox-rich.js
@@ -0,0 +1,17 @@
+function checkboxRich($component, data) {
+  $component.find('.js-checkbox-rich__checkbox')
+    .each((index, item) => {
+      const { name, caption, isChecked } = data[index];
+      $(item).find('.js-checkbox span')
+        .text(caption);
+      $(item).find('.js-checkbox__hidden')
+        .prop('name', name)
+        .prop('checked', isChecked);
+  });
+  $component.find('.js-checkbox-rich__info')
+    .each((index, item) => {
+      item.innerText = data[index].info;
+    });
+}
+
+export default checkboxRich;
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -2,6 +2,9 @@ import DateFilter from '../date-filter/date-filter';
 import DropdownQuantity from '../dropdown-quantity/dropdown-quantity';
 import RangeSlider from '../range-slider/range-slider';
 
+import checkboxRich from '../checkbox-rich/checkbox-rich';
+import checkboxRichData from '../checkbox-rich/checkbox-rich.json';
+
 (function ($) {
   const $filter = $('.js-filter');
 
@@ -14,4 +17,7 @@ import RangeSlider from '../range-slider/range-slider';
   const $dropdownQuantity = $filter.find('.js-dropdown-quantity');
   new DropdownQuantity($dropdownQuantity.eq(0));
   new DropdownQuantity($dropdownQuantity.eq(1), 'room');
+
+  const $checkboxRich = $filter.find('.js-checkbox-rich');
+  checkboxRich($checkboxRich, checkboxRichData);
 }(window.$));

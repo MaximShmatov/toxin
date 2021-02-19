d11a4709d07b835d886b942aa77fc8cd7b0d216b
Maxim Shmatov <ExternGroupMail@mail.ru>
2021-02-19 11:51:02 +0300

    Refactoryng block card-room #15

--- src/components/card-room/card-room.js
+++ src/components/card-room/card-room.js
@@ -13,32 +13,33 @@ import './img/card-room-982-4.jpg';
 
 (function ($) {
   $('.js-card-room').each(function () {
-    const radio = $(this)
-      .find('.js-card-room__slider-radio');
+    const $radio = $(this).find('.js-card-room__slider-radio');
 
     const handleButtonLeftClick = () => {
-      for (let i = 0; i < 4; i += 1) {
-        if ($(radio).eq(i).prop('checked')) {
-          $(radio).eq(i + 1).prop('checked', true);
-          $(radio).eq(i + 1).attr('data-dir', 'l');
-          return;
+      $radio.each((index, item) => {
+        if ($(item).prop('checked')) {
+          $radio.eq(index + 1).prop('checked', true);
+          $radio.eq(index + 1).attr('data-direction', 'left');
+          return false;
         }
-      }
+        return true;
+      });
     };
 
     const handleButtonRightClick = () => {
-      for (let i = 1; i < 5; i += 1) {
-        if ($(radio).eq(i).prop('checked')) {
-          $(radio).eq(i - 1).prop('checked', true);
-          $(radio).eq(i - 1).attr('data-dir', 'r');
-          return;
+      $radio.each((index, item) => {
+        if ($(item).prop('checked') && index > 0) {
+          $radio.eq(index - 1).prop('checked', true);
+          $radio.eq(index - 1).attr('data-direction', 'right');
+          return false;
         }
-      }
+        return true;
+      });
     };
 
     $(this).find('.js-card-room__slider-control-left-button')
-      .on('click.cardroom', handleButtonLeftClick.bind(this));
+      .on('click.cardroom', handleButtonLeftClick);
     $(this).find('.js-card-room__slider-control-right-button')
-      .on('click.cardroom', handleButtonRightClick.bind(this));
+      .on('click.cardroom', handleButtonRightClick);
   });
 }(window.$));
